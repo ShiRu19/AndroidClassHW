f8a35dd56d3ce6c9e55dcb4e9b92ab1217eb7dc6
ShiRu19 <87590345+ShiRu19@users.noreply.github.com>
2022-03-14 21:06:51 +0800

    HW03

---
+++ HW3/107AB0008_HW3_2/app/src/main/java/com/example/android/hellotoast/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    private TextView countText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String count = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        countText = findViewById(R.id.countText);
+        countText.setText(count);
+    }
+}
